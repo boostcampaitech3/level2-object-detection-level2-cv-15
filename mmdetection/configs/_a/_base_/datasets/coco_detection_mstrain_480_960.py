111a94dd1e9efad010feff7f3f39acaf42c74ece
ParkJunghyun <abc1245@naver.com>
2022-03-27 12:51:15 +0000

    UniverseNet

---
+++ mmdetection/configs/_a/_base_/datasets/coco_detection_mstrain_480_960.py
@@ -0,0 +1,58 @@
+# dataset settings
+dataset_type = 'CocoDataset'
+data_root = '../../dataset/'
+classes = ("General trash", "Paper", "Paper pack", "Metal", "Glass", 
+           "Plastic", "Styrofoam", "Plastic bag", "Battery", "Clothing")
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(
+        type='Resize',
+        img_scale=[(512,512), (512,512)],
+        multiscale_mode='range',
+        keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(512,512),
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=32),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+data = dict(
+    samples_per_gpu=2,
+    workers_per_gpu=2,
+    train=dict(
+        classes=classes,
+        type=dataset_type,
+        ann_file=data_root +  'cv_train_1.json',
+        img_prefix=data_root,
+        pipeline=train_pipeline),
+    val=dict(
+        classes=classes,
+        type=dataset_type,
+        ann_file=data_root +  'cv_val_1.json',
+        img_prefix=data_root,
+        pipeline=test_pipeline),
+    test=dict(
+        classes=classes,
+        type=dataset_type,
+        ann_file=data_root + 'test.json',
+        img_prefix=data_root,
+        pipeline=test_pipeline))
+evaluation = dict(interval=1, metric='bbox', classwise=True, save_best="bbox_mAP_50")
