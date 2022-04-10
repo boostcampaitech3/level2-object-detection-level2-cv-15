a56d86f3006aa2f0cce98852dad6048b6cb4109c
TSTP <86605720+seokjin0404@users.noreply.github.com>
2022-04-10 09:58:03 +0900

    update ensemble

---
+++ mmdetection/tools/ensemble/ensemble_config.py
@@ -0,0 +1,10 @@
+{
+    "csvs" : ["./output (3).csv", "./output (2).csv"],
+    "save_path" : "./result_submission2.csv",
+    "ensemble_mode" : "wbf",
+    "weights" : [1, 1],
+    "iou_thr" : 0.6,
+    "skip_box_thr" : 0.0001,
+    "sigma" : 0.1,
+    "img_size" : 1024
+}
---
+++ mmdetection/tools/ensemble/ensenble_config2.py
@@ -0,0 +1,10 @@
+{
+    "csvs" : ["./set2/clean.csv", "./set2/detectors.csv","./set2/swin-multi.csv","./set2/swinL.csv"],
+    "save_path" : "./result_submission3.csv",
+    "ensemble_mode" : "wbf",
+    "weights" : [1,1,1,1],
+    "iou_thr" : 0.6,
+    "skip_box_thr" : 0.0001,
+    "sigma" : 0.1,
+    "img_size" : 1024
+}
